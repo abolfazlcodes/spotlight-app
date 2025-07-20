171a90e4f50c0f4f51220f03df64de3fc96e7655
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-07-20 20:48:41 +0330

    feat: add clerk and authentication to project for loging in

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,66 @@
+import { COLORS } from "@/constants/theme";
+import { styles } from "@/styles/auth.styles";
+import { useSSO } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+export default function Login() {
+  const { startSSOFlow } = useSSO();
+  const router = useRouter();
+
+  const handleGoogleSignIn = async () => {
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy: "oauth_google",
+      });
+
+      if (setActive && createdSessionId) {
+        setActive({
+          session: createdSessionId,
+        });
+        router.push("/(tabs)");
+      }
+    } catch (error) {
+      console.log("oauth_google error: ", error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.brandSection}>
+        <View style={styles.logoContainer}>
+          <Ionicons name="leaf" size={32} color={COLORS.primary} />
+        </View>
+        <Text style={styles.appName}>spotlight</Text>
+        <Text style={styles.tagline}>don&apos;t miss anything</Text>
+      </View>
+
+      <View style={styles.illustrationContainer}>
+        <Image
+          source={require("@/assets/images/auth-bg-3.png")}
+          style={styles.illustration}
+          resizeMode="cover"
+        />
+      </View>
+
+      <View style={styles.loginSection}>
+        <TouchableOpacity
+          style={styles.googleButton}
+          activeOpacity={0.9}
+          onPress={handleGoogleSignIn}
+        >
+          <View style={styles.googleIconContainer}>
+            <Ionicons name="logo-google" size={20} color={COLORS.surface} />
+          </View>
+          <Text style={styles.googleButtonText}>Continue With Google</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.termsText}>
+          By continuing, you agree to our Terms and Privacy Policy.
+        </Text>
+      </View>
+    </View>
+  );
+}
